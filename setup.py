e019b752fecb865f32976b77fa09da33c73111ef
MishaWhite <memantulov@mail.ru>
2023-12-14 23:35:39 +0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='restaurant',
+    version='0.1',
+    description='Test',
+    author='Misha_White',
+    author_email='memantulov@mail.ru',
+    packages=['restaurant'],
+    install_requires=['django', 'djangorestframework'],
+)
+
